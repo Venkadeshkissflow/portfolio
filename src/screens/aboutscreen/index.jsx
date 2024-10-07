fdcc5609c7d31036b5c1bbbdfed4026b1ae896e9
Venkadeshkissflow <venkadesh@kissflow.com>
2024-10-07 23:59:38 +0530

    skill set card updated

--- src/screens/aboutscreen/index.jsx
+++ src/screens/aboutscreen/index.jsx
@@ -8,72 +8,107 @@ import Aspire from "../../images/Aspire.png"
 import { DiReact } from "react-icons/di";
 import { DiJavascript1 } from "react-icons/di";
 import { DiHtml5 } from "react-icons/di";
-import {FaCss3Alt} from "react-icons/fa"
+import {FaCss3Alt} from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+
 
 import "./styles.css";
 import "../../commonstyle.css"
 
+// export const AboutScreen = React.forwardRef((props, ref)=>{
+//     return(
+//         <div ref={ref} className="aboutScreenContainer">
+//             <Card className="aboutScreenCard">
+//             <div>
+//                 <p className="fontStyle fontTitleStyle">About</p>
+//                 <p className="fontStyle">
+//                     Hi, I'm Venkadesh S, a self-taught passionate FrontEnd developer from India, 
+//                     currently working at Kissflow 🦋 as a FrontEnd engineer.I passionately combine 
+//                     good design, technology, and innovation in all my projects, My goal is to build 
+//                     highly performant applications that solve real-world problems and provide users with an awesome experience.
+//                 </p>
+
+//                 <div className="skillsBoard">
+//                 <p className="fontStyle fontTitleStyle skills">Skill Sets</p>
+//                 <div className="skillsContainer">
+//                 <span className="IconContainer reactIcon">
+//                         <DiReact className="Icon"/>
+//                     </span>
+//                     <span className="IconContainer jsIcon">
+//                         <DiJavascript1 className="Icon"/>
+//                     </span>
+//                     <span className="IconContainer htmlIcon">
+//                         <DiHtml5 className="Icon"/>
+//                     </span>
+//                     <span className="IconContainer cssIcon">
+//                         <FaCss3Alt className="Icon"/>
+//                     </span>
+//                 </div>
+//                 </div>
+//             </div>
+
+//             <div>
+//                 <p className="fontStyle fontTitleStyle">Experiances</p>
+//                 <div className="overallExperiance">
+//                     <div className="firstPart start">
+//                         <div className="exp-circle">
+//                            <div className="starttingPoint"></div>
+//                         </div>
+//                         <p className="exp-line fontStyle">intern</p>
+//                     </div>
+//                     <div className="secondPart">
+//                         <div className="exp-circle">
+//                         <img className="companyimages" src={Aspire} alt=""/>
+//                         </div>
+//                         <p className="exp-line fontStyle">Asociative Software Engineer</p>
+//                     </div>
+//                     <div className="ThirdPart">
+//                         <div className="exp-circle">
+//                         <img className="companyimages" src={Kissflow} alt=""/>
+//                         </div>
+//                         <p className="exp-line fontStyle">Software Engineer</p>
+//                     </div>
+//                     <div className="FourthPart">
+//                         <div className="exp-circle">
+//                         <img className="companyimages" src={Kissflow} alt=""/>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             </Card>
+//         </div>
+//     )
+// })
+
 export const AboutScreen = React.forwardRef((props, ref)=>{
-    return(
-        <div ref={ref} className="aboutScreenContainer">
-            <Card className="aboutScreenCard">
-            <div>
-                <p className="fontStyle fontTitleStyle">About</p>
-                <p className="fontStyle">
-                    Hi, I'm Venkadesh S, a self-taught passionate FrontEnd developer from India, 
-                    currently working at Kissflow 🦋 as a FrontEnd engineer.I passionately combine 
-                    good design, technology, and innovation in all my projects, My goal is to build 
-                    highly performant applications that solve real-world problems and provide users with an awesome experience.
-                </p>
+    return (
+    <div className="skillsWrapper">
+        <Card className="skillset reactjs">
+            <DiReact className="skillIcon "/>
+            <span className="fontStyle">ReactJS</span>
+        </Card>
+        <Card className="skillset js">
+            <DiJavascript1 className="skillIcon "/>
+            <span className="fontStyle">Java script</span>
+        </Card>
+        <Card className="skillset html">
+            <DiHtml5 className="skillIcon "/>
+            <span className="fontStyle">HTML</span>
+        </Card>
 
-                <div className="skillsBoard">
-                <p className="fontStyle fontTitleStyle skills">Skill Sets</p>
-                <div className="skillsContainer">
-                <span className="IconContainer reactIcon">
-                        <DiReact className="Icon"/>
-                    </span>
-                    <span className="IconContainer jsIcon">
-                        <DiJavascript1 className="Icon"/>
-                    </span>
-                    <span className="IconContainer htmlIcon">
-                        <DiHtml5 className="Icon"/>
-                    </span>
-                    <span className="IconContainer cssIcon">
-                        <FaCss3Alt className="Icon"/>
-                    </span>
-                </div>
-                </div>
-            </div>
-
-            <div>
-                <p className="fontStyle fontTitleStyle">Experiances</p>
-                <div className="overallExperiance">
-                    <div className="firstPart start">
-                        <div className="exp-circle">
-                           <div className="starttingPoint"></div>
-                        </div>
-                        <p className="exp-line fontStyle">intern</p>
-                    </div>
-                    <div className="secondPart">
-                        <div className="exp-circle">
-                        <img className="companyimages" src={Aspire} alt=""/>
-                        </div>
-                        <p className="exp-line fontStyle">Asociative Software Engineer</p>
-                    </div>
-                    <div className="ThirdPart">
-                        <div className="exp-circle">
-                        <img className="companyimages" src={Kissflow} alt=""/>
-                        </div>
-                        <p className="exp-line fontStyle">Software Engineer</p>
-                    </div>
-                    <div className="FourthPart">
-                        <div className="exp-circle">
-                        <img className="companyimages" src={Kissflow} alt=""/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </Card>
-        </div>
+        <Card className="skillset css">
+            <FaCss3Alt className="skillIcon "/>
+            <span className="fontStyle">CSS</span>
+        </Card>
+        <Card className="skillset tailwind">
+            <RiTailwindCssFill className="skillIcon "/>
+            <span className="fontStyle">Tailwind CSS</span>
+        </Card>
+        <Card className="skillset node">
+                 <FaNodeJs className="skillIcon "/>
+                 <span className="fontStyle">Node JS</span>
+        </Card>
+    </div>
     )
 })
