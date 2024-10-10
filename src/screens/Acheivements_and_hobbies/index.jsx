fb40595edd694d8eae4c562d38f84c1047719de6
Venkadeshkissflow <venkadesh@kissflow.com>
2024-10-10 16:38:35 +0530

    project section added

--- src/screens/Acheivements_and_hobbies/index.jsx
+++ src/screens/Acheivements_and_hobbies/index.jsx
@@ -2,12 +2,6 @@ import React from "react";
 
 import {Card} from "../../components/card/index";
 
-import CountryHen from "../../images/countryhen.png";
-import chat from "../../images/chats.jpg";
-import pots from "../../images/pots.jpg";
-import sportcertificate from "../../images/certificate2.webp";
-import projectCertificate from "../../images/certificate.webp";
-import ArtCertificate from "../../images/certificateArt.png";
 import Award from "../../images/awards.png"
 
 import "swiper/css";
@@ -18,50 +12,6 @@ import "./styles.css";
 
 import "../../commonstyle.css"
 
-import { EffectCoverflow, Keyboard , Navigation, Autoplay, Mousewheel, EffectFade, EffectCards } from "swiper";
-
-const SELF_DETAILS = [
-{
-  title: "Country Hen [ REACT NATIVE ]",
-  img: CountryHen,
-  detailInfo:"An ecommerce application user interface is developed for the meat and eggs of countryhen with react native",
-  tools: "VS Code, Postman , MongoDB Cloud",
-  role: "Frontend Developer"
-
-},
-{
-  title: "Chat App [ Socket io ]",
-  img: chat,
-  detailInfo:"In this chat App, we can transfer messages from one to one as personally. here I use socket-io for connecting users.",
-  tools: "JavaScript, HTML, CSS, SocketIO",
-  role: "Developer"
-},
-{
-  title: "Star Performer",
-  img: Award,
-  detailInfo:"Got Star performer award in kissflow for inter team peformance"
-},
-{
-    title: "First Prize in Mini Project",
-    img: projectCertificate,
-    detailInfo:"Won First Prize in Mini Project event held on intra college event at kongunadu college of engineering and technology trichy."
-},
-{
-  title: "sports",
-  img: sportcertificate,
-  detailInfo:"like to play cricket, football, balbadminton, got an journal level certification in balbadminton"
-},
-{
-  title: "Art",
-  img: ArtCertificate,
-  detailInfo:"Participated junior level in National Art Competion in School days"
-},
-{
-    title: "love gardening",
-    img: pots,
-    detailInfo:"Apart from technical love to spend my time in gardening and home decorations"
-},
-]
 
 const AWARDS = [
   {
--- src/screens/projects/index.jsx
+++ src/screens/projects/index.jsx
@@ -1,7 +1,89 @@
 import React from "react";
 
+import { Card } from "../../components/card";
+
+import { DiReact } from "react-icons/di";
+import { DiJavascript1 } from "react-icons/di";
+import { DiHtml5 } from "react-icons/di";
+import {FaCss3Alt} from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiSocketdotio } from "react-icons/si";
+
+import CountryHen from "../../images/projects/countryhen.png";
+import ChatApplication from "../../images/projects/chatapplication.png";
+import Game from "../../images/projects/game.png";
+import KNCETian from "../../images/projects/kncet.jpg"
+
+
+import "./styles.css"
+
+const PROJECTS = [
+    {
+        TITLE: "Country Hen [ REACT NATIVE ]",
+        DESCRIPTIONS: "An ecommerce application user interface is developed for the meat and eggs of countryhen with react native",
+        LANGUAGES: [
+            DiHtml5, 
+            FaCss3Alt,
+            DiReact,
+        ],
+        IMG: CountryHen
+    },
+    {
+        TITLE: "KNCETians [ REACT NATIVE ]",
+        DESCRIPTIONS: "In this Mobile Application which is used for building connectivity between Alumni and the college.",
+        LANGUAGES: [
+            DiHtml5,
+            FaCss3Alt,
+            DiReact,
+        ],
+        IMG: KNCETian
+    },
+    {
+        TITLE: "Chat App [ Socket io ]",
+        DESCRIPTIONS: "In this chat App, we can transfer messages from one to one as personally. here I use socket-io for connecting users.",
+        LANGUAGES: [
+            DiHtml5, 
+            FaCss3Alt,
+            DiJavascript1,
+            FaNodeJs,
+            SiSocketdotio
+        ],
+        IMG: ChatApplication
+    },
+    {
+        TITLE: "Stone paper scissor game",
+        DESCRIPTIONS: "Stone-Paper-Scissors is a simple game where players choose between stone, paper, or scissors, each option defeating or losing to another.",
+        LANGUAGES: [
+            DiHtml5, 
+            FaCss3Alt,
+            DiJavascript1,
+        ],
+        IMG: Game
+    }
+]
+
 export function ProjectSection(){
     return (
-        <div>project</div>
+        <div className="projectWrapper">
+            {
+                PROJECTS.map(({TITLE,DESCRIPTIONS,  LANGUAGES, IMG})=>(
+                    <div className="projectSection">
+                    <Card className="projectImageWrapper">
+                        <img className="projectImage" alt="projectImage"  src={IMG} />
+                    </Card>
+                    <div className="infoWrapper">
+                        <div className="fontStyle title infoSection">{TITLE}</div>
+                        <div className="fontStyle descriptions infoSection">{DESCRIPTIONS}</div>
+                        <div className="iconsWrapper infoSection">
+                            {
+                                LANGUAGES.map((Language)=><Language className="languageIcon" />)
+                            }
+                        </div>
+                    </div>
+                </div>
+                ))
+            }
+        </div>
     )
 }
